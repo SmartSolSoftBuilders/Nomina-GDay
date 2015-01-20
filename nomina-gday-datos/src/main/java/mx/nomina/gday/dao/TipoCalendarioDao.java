ae64c2f3b7daed0b8309ffdc27768b631c921a4e
alf-otika <alf.otika@gmail.com>
2015-01-19 19:53:22 -0600

    TipoCalendarioDAO

---
+++ nomina-gday-datos/src/main/java/mx/nomina/gday/dao/TipoCalendarioDao.java
@@ -0,0 +1,12 @@
+package mx.nomina.gday.dao;
+
+import java.util.List;
+
+import mx.nomina.gday.modelo.Esquema;
+import mx.nomina.gday.modelo.TipoCalendario;
+
+public interface TipoCalendarioDao {
+
+	public List<TipoCalendario> obtenerTiposCalendario();
+
+}
